0bfaee39c53a0c02f82441ed1ee1d92de2f14b5d
haruperi <r.haruperi@hotmail.com>
2022-01-18 14:00:35 +0200

    Added Bank

--- src/com/haruperi/Main.java
+++ src/com/haruperi/Main.java
@@ -3,6 +3,6 @@ package com.haruperi;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to The Mortgage Calculator");
+        System.out.println("Welcome to The Bank Mortgage Calculator");
     }
 }
